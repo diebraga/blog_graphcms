e587c8c4e23bcb9dc2ce7341f325868947a14776
diebraga <diebraga0055@gmail.com>
2021-12-04 14:43:22 +0000

    full postcard

--- @types/index.ts
+++ @types/index.ts
@@ -4,8 +4,18 @@ export type BlogPost = {
     title: string
     exerpt: string
     slug: string  
+    author: Author
+    createdAt: string
     banner: {
       url: string
     }
   }
+}
+
+export type Author = {
+  name: string
+  avatar: {
+    url: string
+  }
+  id: string
 }
